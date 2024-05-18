f94b8cbfaa18e32f866b9ecaa647f3236c26f0cf
MisbahKhan0009 <misbah.khan@northsouth.edu>
2024-05-18 15:50:42 +0600

    Preloader Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes/Routes";
+import Preloader from "./Pages/Loaders/Preloader/Preloader";
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router}  />
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Simulate a loading process (e.g., fetching data)
+    const loadData = async () => {
+      // Simulate a network request
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      setLoading(false);
+    };
+
+    loadData();
+  }, []);
+
+  return <>{loading ? <Preloader /> : <RouterProvider router={router} />}</>;
 }
 
 export default App;
