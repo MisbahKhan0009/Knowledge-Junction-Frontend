78ec992baeb666185bae5ea156ae442e019795a4
MisbahKhan0009 <misbah.khan@northsouth.edu>
2024-05-22 20:06:52 +0600

    Preloader Updated

--- src/Pages/Loaders/Preloader/Preloader.jsx
+++ src/Pages/Loaders/Preloader/Preloader.jsx
@@ -4,32 +4,34 @@ import SplitType from "split-type";
 import "./Preloader.css";
 
 const Preloader = () => {
-  useEffect(() => {
-    const myText = new SplitType("#my-text", { types: "chars" });
-
-    const animateText = () => {
-      gsap.to(".char", {
-        y: 0,
-        stagger: 0.05,
-        delay: 0.2,
-        duration: 0.1,
-        onComplete: () => {
-          gsap.to(".char", {
-            y: 115,
-            delay: 0.5,
-            duration: 0.5,
-            onComplete: animateText,
-          });
-        },
-      });
-    };
-
-    animateText();
-  }, []);
+  // useEffect(() => {
+  //   if (document.querySelector(".char")) {
+  //     const myText = new SplitType("#my-text", { types: "chars" });
+  //     const animateText = () => {
+  //       gsap.to(".char", {
+  //         y: 0,
+  //         stagger: 0.05,
+  //         delay: 0.2,
+  //         duration: 0.1,
+  //         onComplete: () => {
+  //           gsap.to(".char", {
+  //             y: 115,
+  //             delay: 0.5,
+  //             duration: 0.5,
+  //             onComplete: animateText,
+  //           });
+  //         },
+  //       });
+  //     };
+  //     if (document.querySelector(".char")) {
+  //       animateText();
+  //     }
+  //   }
+  // }, []);
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 id="my-text" className="text-primary font-bold uppercase">
+    <div className="flex justify-center items-center h-screen ">
+      <h1 id="my-text" className="text-primary font-bold uppercase animate-pulse">
         Knowledge Junction
       </h1>
     </div>
