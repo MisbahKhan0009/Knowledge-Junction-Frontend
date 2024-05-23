400e72fd25b0008878daca167b3a8bb5f9024c16
MisbahKhan0009 <misbah.khan@northsouth.edu>
2024-05-23 18:12:17 +0600

    Bug fixed

--- src/Pages/Books/SingleBook/SingleBooks.jsx
+++ src/Pages/Books/SingleBook/SingleBooks.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { FaBookSkull } from "react-icons/fa6";
 
 const SingleBooks = () => {
   const { ISBN } = useParams();
@@ -14,12 +15,24 @@ const SingleBooks = () => {
         console.log(res.data);
       })
       .catch((err) => console.error(err));
-  }, []);
+  }, [ISBN]);
 
   return (
     <section>
-      <div>
-        <h1>{book.Title}</h1>
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+            alt="Album"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{book.Title}</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div>
+        </div>
       </div>
       <div></div>
     </section>
